56b3c3ccc6ad23b8985a706811656723b5d962e9
sooshe <sooshe@sooshebot.com>
2019-10-31 12:30:33 -0500

    add validateuser, userid, and post to userRouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,13 +1,33 @@
 const express = require('express');
 
 const router = express.Router();
+const Post = require('../posts/postDb');
+const User = require('../users/userDb');
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, async (req, res) => {
+    const userName = req.body;
 
+    try{
+        const name = await User.insert(userName);
+        res.status(201).json(name);
+    }
+    catch(err) {
+        res.status(500).json({error: 'There was an error in adding a new user.'})
+    }
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
+    const {id} = req.user;
+    const {text} = req.body;
+    console.log(text, id);
 
+    try {
+        const newPost = await Post.insert({ text, user_id:id})
+        res.status(201).json(newPost)
+    }
+    catch(err) {
+        res.status(500).json({error: 'There was an error while saving post to the database.'})
+    }
 });
 
 router.get('/', (req, res) => {
