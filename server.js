74256b884d3b2299155fb7ad0eae72112faa92e8
sooshe <sooshe@sooshebot.com>
2019-10-30 22:18:11 -0500

    install helmet

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
+const helmet = require('helmet');
 
-// const helmet = require('helmet');
-// const morgan = require('morgan')
 
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter');
