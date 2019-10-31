b43d20fcddf86fcb0af8851cca1735a723be467a
sooshe <sooshe@sooshebot.com>
2019-10-31 12:21:15 -0500

    add express, helmet and the routers to server file

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const server = express();
 const helmet = require('helmet');
-
-
 const postRouter = require('./posts/postRouter');
 const userRouter = require('./users/userRouter');
 
-const server = express();
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use('./api.users', userRouter);
+server.use('./api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
