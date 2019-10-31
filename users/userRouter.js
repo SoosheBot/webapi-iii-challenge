508a979b1a7d0e2c5d277c6bd788cbc704c8dac8
sooshe <sooshe@sooshebot.com>
2019-10-31 12:35:55 -0500

    add validate user by id to userrouter

--- users/userRouter.js
+++ users/userRouter.js
@@ -30,12 +30,29 @@ router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
     }
 });
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.get();
+        res.status(200).json(users)
+    }
+    catch (err) {
+        res.status(500).json({error: 'The users could not be retrieved.'})
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, async (req, res) => {
+    const {id} = req.user;
+    try {
+       const user = await User.getById(id)
+       if (user) {
+           res.status(200).json(user);
+       } else {
+           res.status(404).json({error: 'The user with that specified ID does not exist.'})
+       }   
+    }
+    catch (err) {
+        res.status(500).json({error: 'The user information could not be retrieved.' })
+    }
 });
 
 router.get('/:id/posts', (req, res) => {
